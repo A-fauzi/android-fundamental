0c6494d0ccaaf5f87c0a0b73f6bca17ac7d84009
A-fauzi <akhmadfauzi0101@gmail.com>
2022-09-29 13:44:55 +0700

    activity lifecycle

--- app/src/main/java/com/example/android_fundamental/MainActivity.kt
+++ app/src/main/java/com/example/android_fundamental/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.android_fundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
     }
 }
